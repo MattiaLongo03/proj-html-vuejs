1a76c9d1246b196225ddc1697888a5a9576d58a6
MattiaLongo03 <mattilongo2003@gmail.com>
2022-12-05 17:53:05 +0100

    Struttura Header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
+    <HeaderPage />
     <MainPage />
   </div>
 </template>
 
 <script>
+import HeaderPage from '@/components/HeaderPage.vue';
 import MainPage from '@/components/MainPage.vue';
 
 export default {
   name: 'App',
   components: {
+    HeaderPage,
     MainPage,
   },
 };
--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,13 +1,45 @@
+<!-- eslint-disable max-len -->
 <template>
-  <div />
+  <header>
+    <div class="container">
+      <img
+        src="../assets/images/white-logo-2.png"
+        alt=""
+      >
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Blog</li>
+          <li>Contact</li>
+          <li>Portfolio</li>
+        </ul>
+        <button>Sign In</button>
+      </nav>
+    </div>
+    <div>
+      <div class="container">
+        <h2>Business Planning Digital Technology Modern Solution</h2>
+        <p>When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper surface</p>
+        <button>Try Now</button>
+        <button>Explore More</button>
+      </div>
+      <div class="container">
+        <img
+          src="../assets/images/header.png"
+          alt=""
+        >
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
-
+  name: 'HeaderPage',
 };
 </script>
 
-<style>
-
+<style lang="scss" scoped>
 </style>
