c10c82921f1346211e39c9e8f288891465f07b45
MattiaLongo03 <mattilongo2003@gmail.com>
2022-12-06 16:20:25 +0100

    header completato

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,38 +1,50 @@
 <!-- eslint-disable max-len -->
 <template>
-  <header>
-    <div class="container">
-      <img
-        src="../assets/images/white-logo-2.png"
-        alt=""
-      >
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Services</li>
-          <li>Blog</li>
-          <li>Contact</li>
-          <li>Portfolio</li>
-        </ul>
-        <button>Sign In</button>
-      </nav>
-    </div>
-    <div>
-      <div class="container">
-        <h2>Business Planning Digital Technology Modern Solution</h2>
-        <p>When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper surface</p>
-        <button>Try Now</button>
-        <button>Explore More</button>
-      </div>
-      <div class="container">
+  <div>
+    <div class="container pt-3">
+      <header>
         <img
-          src="../assets/images/header.png"
+          src="../assets/images/white-logo-2.png"
           alt=""
         >
-      </div>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Blog</li>
+            <li>Contact</li>
+            <li>Portfolio</li>
+          </ul>
+          <button id="blue">
+            Sign In
+          </button>
+        </nav>
+      </header>
     </div>
-  </header>
+    <div class="container business">
+      <div class="container business-testo">
+        <h2>Business Planning Digital Technology Modern Solution !</h2>
+        <p id="sub">
+          When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper surface
+        </p>
+        <button
+          id="red"
+          style="margin-right:10px"
+        >
+          Try it Now
+        </button>
+        <button id="blue">
+          Explore More
+        </button>
+      </div>
+      <img
+        id="desk"
+        src="../assets/images/header.png"
+        alt=""
+      >
+    </div>
+  </div>
 </template>
 
 <script>
@@ -42,4 +54,66 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+*{
+  color: white;
+  background-color: #030636;
+}
+
+#blue{
+  padding: 5px 30px;
+  background-color: #1466A1;
+  border-radius: 25px;
+}
+
+#red{
+  padding: 5px 30px;
+  background-color: #DA466C;
+  border-radius: 25px;
+}
+
+header{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  nav{
+    display: flex;
+  }
+  ul{
+    margin: 0 50px;
+    display: flex;
+    list-style: none;
+  }
+
+  li{
+    font-weight: bold;
+    padding: 10px;
+    display: flex;
+    cursor: pointer;
+  }
+
+  li:hover{
+    color: #DA466C;
+  }
+}
+
+.business{
+  display: flex;
+  align-items: center;
+  height: 500px;
+
+  .business-testo{
+    margin-left: 0;
+    width: 350px;
+    #sub{
+      margin: 30px 0 ;
+      width: 400px;
+      color: #03A7D3E6;
+    }
+  }
+
+  #desk{
+    width: 500px;
+  }
+}
 </style>
