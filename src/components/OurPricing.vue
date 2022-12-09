7a04404b3be45d6ba476861005b0e4a209ea046e
MattiaLongo03 <mattilongo2003@gmail.com>
2022-12-09 15:28:21 +0100

    Modifica OurPricing

--- src/components/OurPricing.vue
+++ src/components/OurPricing.vue
@@ -7,58 +7,58 @@
       </p>
       <h2>Our Pricing</h2>
       <p>When, while the lovely valley teems with vapour aorund meand the merdian sun strikes the upper surface.</p>
-    </div>
-    <div class="container schede">
-      <div class="scheda">
-        <h2>Designing</h2>
-        <p>PROCESS</p>
-        <p>$</p>
-        <h1>40</h1>
-        <p>Per Month</p>
-        <p><span style="font-weight:bold">Creative</span> Design Enabled</p>
-        <p><span style="font-weight:bold">Vibrant</span> Color Usage</p>
-        <p><span style="font-weight:bold">Eye</span> Catching Design</p>
-        <p><span style="font-weight:bold">Extreme</span> Typography</p>
-        <p><span style="font-weight:bold">Exceptional</span> Design</p>
-        <button id="red">
-          Order Now
-          <font-awesome-icon icon="fa-solid fa-arrow-right" />
-        </button>
-      </div>
-      <div class="schedared">
-        <h2>Developing</h2>
-        <p>PRODUCT</p>
-        <p>$</p>
-        <h1>60</h1>
-        <p>Per Month</p>
-        <p><span style="font-weight:bold">Creative</span> Design Enabled</p>
-        <p><span style="font-weight:bold">Vibrant</span> Color Usage</p>
-        <p><span style="font-weight:bold">Eye</span> Catching Design</p>
-        <p><span style="font-weight:bold">Extreme</span> Typography</p>
-        <p><span style="font-weight:bold">Exceptional</span> Design</p>
-        <button
-          id="red"
-          style="background-color:white; color:#F9636B"
-        >
-          Order Now
-          <font-awesome-icon icon="fa-solid fa-arrow-right" />
-        </button>
-      </div>
-      <div class="scheda">
-        <h2>Supporting</h2>
-        <p>CLIENTS</p>
-        <p>$</p>
-        <h1>80</h1>
-        <p>Per Month</p>
-        <p><span style="font-weight:bold">Creative</span> Design Enabled</p>
-        <p><span style="font-weight:bold">Vibrant</span> Color Usage</p>
-        <p><span style="font-weight:bold">Eye</span> Catching Design</p>
-        <p><span style="font-weight:bold">Extreme</span> Typography</p>
-        <p><span style="font-weight:bold">Exceptional</span> Design</p>
-        <button id="red">
-          Order Now
-          <font-awesome-icon icon="fa-solid fa-arrow-right" />
-        </button>
+      <div class="container schede">
+        <div class="scheda">
+          <h2>Designing</h2>
+          <p>PROCESS</p>
+          <p>$</p>
+          <h1>40</h1>
+          <p>Per Month</p>
+          <p><span style="font-weight:bold">Creative</span> Design Enabled</p>
+          <p><span style="font-weight:bold">Vibrant</span> Color Usage</p>
+          <p><span style="font-weight:bold">Eye</span> Catching Design</p>
+          <p><span style="font-weight:bold">Extreme</span> Typography</p>
+          <p><span style="font-weight:bold">Exceptional</span> Design</p>
+          <button id="red">
+            Order Now
+            <font-awesome-icon icon="fa-solid fa-arrow-right" />
+          </button>
+        </div>
+        <div class="schedared">
+          <h2>Developing</h2>
+          <p>PRODUCT</p>
+          <p>$</p>
+          <h1>60</h1>
+          <p>Per Month</p>
+          <p><span style="font-weight:bold">Creative</span> Design Enabled</p>
+          <p><span style="font-weight:bold">Vibrant</span> Color Usage</p>
+          <p><span style="font-weight:bold">Eye</span> Catching Design</p>
+          <p><span style="font-weight:bold">Extreme</span> Typography</p>
+          <p><span style="font-weight:bold">Exceptional</span> Design</p>
+          <button
+            id="red"
+            style="background-color:white; color:#F9636B"
+          >
+            Order Now
+            <font-awesome-icon icon="fa-solid fa-arrow-right" />
+          </button>
+        </div>
+        <div class="scheda">
+          <h2>Supporting</h2>
+          <p>CLIENTS</p>
+          <p>$</p>
+          <h1>80</h1>
+          <p>Per Month</p>
+          <p><span style="font-weight:bold">Creative</span> Design Enabled</p>
+          <p><span style="font-weight:bold">Vibrant</span> Color Usage</p>
+          <p><span style="font-weight:bold">Eye</span> Catching Design</p>
+          <p><span style="font-weight:bold">Extreme</span> Typography</p>
+          <p><span style="font-weight:bold">Exceptional</span> Design</p>
+          <button id="red">
+            Order Now
+            <font-awesome-icon icon="fa-solid fa-arrow-right" />
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -84,8 +84,9 @@ export default {
     text-align: center;
 }
 .scheda{
+    color: black;
     padding: 50px;
-    background-color: lightgrey;
+    background-color: white;
     border-radius: 30px;
 }
 .schedared{
